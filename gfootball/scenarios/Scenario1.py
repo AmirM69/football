2b7d787e1110140f342e28cbaeb0db88beb3df23
Amir Chegoonian <56601827+AmirM69@users.noreply.github.com>
2020-10-07 00:54:26 -0400

    Create Scenario1.py

---
+++ gfootball/scenarios/Scenario1.py
@@ -0,0 +1,18 @@
+from . import *
+
+def build_scenario(builder):
+  builder.config().game_duration = 300
+  builder.config().deterministic = False
+  builder.config().offsides = False
+  builder.config().end_episode_on_score = True
+  builder.config().end_episode_on_out_of_play = True
+  builder.config().end_episode_on_possession_change = True
+  builder.SetBallPosition(0.5, 0.0)
+
+  builder.SetTeam(Team.e_Left)
+  builder.AddPlayer(-1.0, 0.0, e_PlayerRole_GK)
+  builder.AddPlayer(0.4, 0.0, e_PlayerRole_CF)
+
+  builder.SetTeam(Team.e_Right)
+  builder.AddPlayer(-1.0, 0.0, e_PlayerRole_GK)
+  builder.AddPlayer(-0.7, 0.0, e_PlayerRole_CB)
